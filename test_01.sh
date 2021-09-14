a51227d0132a60381f4e034da48c73fb25fdc6f9
yinan <yinan.a.zhou@gmail.com>
2021-09-14 12:01:45 -0700

    new error

--- test_01.sh
+++ test_01.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-ichiro
 #SBATCH --time=20:00
-#SBATCH --output=run_output/test_output.out
+#SBATCH --output=run_output/test_output_01.out
 #SBATCH --gres=gpu:v100:1
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
@@ -11,8 +11,9 @@ module load python/3.8
 module load scipy-stack
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
-pip install tensorflow
-pip install torch
+pip3 install --upgrade --no-binary numpy==1.20.0 numpy==1.20.0
+pip install --no-index -r requirements.txt
 
-echo "Starting Task $SLURM_ARRAY_TASK_ID"
-python -u test.py
+
+echo "Starting Task"
+python test.py
