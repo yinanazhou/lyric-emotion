66be8cf04ec9d1472cda24b8d9d0ac2de2849cdf
yinan <yinan.a.zhou@gmail.com>
2021-10-10 18:49:25 -0700

    StopIteration error

--- test_02.sh
+++ test_02.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --account=def-ichiro
-#SBATCH --time=01:00:00
-#SBATCH --output=run_output/test_output_01.out
+#SBATCH --time=00:30:00
+#SBATCH --output=run_output/test_output_02.out
 #SBATCH --gres=gpu:v100:1
-#SBATCH --gres=gpu:2       # Request GPU "generic resources"
+#SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=15G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 
-module load python/3.8
+module load python/3.7
 module load scipy-stack
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
