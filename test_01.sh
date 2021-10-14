a091acb61b01504215c0ed45bd9428bf780183a5
yinan <yinan.a.zhou@gmail.com>
2021-10-14 11:22:42 -0700

    cuda out of memory

--- test_01.sh
+++ test_01.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=01:00:00
 #SBATCH --output=run_output/test_output_01.out
 #SBATCH --gres=gpu:v100:1
-#SBATCH --gres=gpu:4       # Request GPU "generic resources"
+#SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=15G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 
