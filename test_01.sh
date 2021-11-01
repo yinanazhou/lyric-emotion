60a702cca71aacde8a42ca76a7a16d314c5b924b
yinan <yinan.a.zhou@gmail.com>
2021-11-01 07:20:12 -0700

    bash update

--- test_01.sh
+++ test_01.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:v100:1
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=15G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --mem=150G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 
 module load python/3.8
 module load scipy-stack
