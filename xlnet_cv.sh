e6e27cac06d5e3df98604a1c4f07787f488623ee
yinan <yinan.a.zhou@gmail.com>
2021-11-14 18:53:30 -0800

    result bs 16

--- xlnet_cv.sh
+++ xlnet_cv.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=run_output/xlnet_cv_output_02.out
 #SBATCH --gres=gpu:v100:1
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
-#SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=150G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 
 module load python/3.8
@@ -18,4 +18,4 @@ pip install --no-index -r requirements.txt
 
 
 echo "Starting Task"
-python xlnet_cv.py --ml 512 --bs 16
+python xlnet_cv.py --ml 512 --bs 32
