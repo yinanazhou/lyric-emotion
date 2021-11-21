d9bbbe15d2f57e9cc97a357c3ad861f7b229f2a5
Yinan Zhou <yinan.a.zhou@gmail.com>
2021-11-20 20:02:07 -0500

    bs 8 cpu 1

--- xlnet_bs_8_es_5.sh
+++ xlnet_bs_8_es_5.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=run_output/xlnet_cv_output_%A_%a.out
 #SBATCH --gres=gpu:v100:1
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
-#SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --array=1-6
 #SBATCH --mem=200G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 
