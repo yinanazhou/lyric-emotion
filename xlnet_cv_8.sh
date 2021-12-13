fff65ba360c693a5a532fc22c0d057104484bfc7
yinan <yinan.a.zhou@gmail.com>
2021-12-13 07:11:22 -0800

    results part 1

--- xlnet_cv_8.sh
+++ xlnet_cv_8.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:v100:1
 #SBATCH --gres=gpu:4       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=8G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --mem=4G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 
 module load nixpkgs/16.09
 module load python/3.8.2
@@ -20,4 +20,4 @@ pip install --no-index wandb
 wandb login $API_KEY
 
 echo "Starting Task"
-python xlnet_cv.py --ml 256 --bs 8 --epochs 500 --lr 4 --es 4
+python xlnet_cv.py --ml 256 --bs 8 --epochs 500 --lr 4 --es 2
