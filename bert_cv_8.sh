14f941c17873d594c1e7228a8efadb6294c999b9
yinan <yinan.a.zhou@gmail.com>
2021-12-24 14:06:36 -0800

    results

--- bert_cv_8.sh
+++ bert_cv_8.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:v100:1
 #SBATCH --gres=gpu:4       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=1G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --mem=4G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 
 module load nixpkgs/16.09
 module load python/3.8.2
