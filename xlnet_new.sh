72f7decfe3b99c87bf527d0fce66cec7b67566c5
yinan <yinan.a.zhou@gmail.com>
2022-01-09 20:54:14 -0800

    xlnet 512 0.005 result

--- xlnet_new.sh
+++ xlnet_new.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --account=def-ichiro
-#SBATCH --time=2-00:00:00
-#SBATCH --output=run_output/xlnet_cv_output_lr5_es_0.005_%A_%a.out
-#SBATCH --gres=gpu:v100l:1
-#SBATCH --gres=gpu:4       # Request GPU "generic resources"
+#SBATCH --time=6-00:00:00
+#SBATCH --output=run_output/xlnet_cv_512_lr4_output_%A_%a.out
+#SBATCH --gres=gpu:v100l:4
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --array=2,3,4,5
 #SBATCH --mem=4G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 
 module load nixpkgs/16.09
@@ -20,4 +20,5 @@ pip install --no-index wandb
 wandb login $API_KEY
 
 echo "Starting Task"
-python xlnet_esnum.py --ml 512 --bs 8 --epochs 500 --lr 5 --es 0.005
+python xlnet_cv.py --ml 512 --bs 8 --epochs 500 --lr 4 --es $SLURM_ARRAY_TASK_ID
+
