f9d28b5972b189c382d1b1d35a8269a32b1e81aa
yinan <yinan.a.zhou@gmail.com>
2022-01-20 20:54:06 -0800

    weird result

--- bert_nr_lemma.sh
+++ bert_nr_lemma.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --account=def-ichiro
+#SBATCH --account=rpp-ichiro
 #SBATCH --time=5-00:00:00
 #SBATCH --output=run_output/bert_nr_lemma_%A.out
-#SBATCH --gres=gpu:v100:1
-#SBATCH --gres=gpu:4       # Request GPU "generic resources"
+#SBATCH --gres=gpu:v100l:4
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=8G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --mem=4G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 
 module load nixpkgs/16.09
 module load python/3.8.2
