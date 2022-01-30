6a914401d0388fdf17995895ed92a9ada0ae4a40
yinan <yinan.a.zhou@gmail.com>
2022-01-30 06:50:59 -0800

    error message

--- bert_bs_8_es_3.sh
+++ bert_bs_8_es_3.sh
@@ -3,7 +3,6 @@
 #SBATCH --time=5-00:00:00
 #SBATCH --output=run_output/bert_cv_512_lr5_output_%A_%a.out
 #SBATCH --gres=gpu:v100l:4
-#SBATCH --gres=gpu:4       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --array=2,3,4,5
 #SBATCH --mem=4G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
