fb85f1d8d6fe9d70973513fdb5e5a66f940a5c11
yinan <yinan.a.zhou@gmail.com>
2022-02-08 20:15:20 -0800

    results 1st batch

--- xlnet_cv.py
+++ xlnet_cv.py
@@ -6,7 +6,7 @@ import string
 import torch
 import numpy as np
 import logging
-from transformers import XLNetTokenizer, XLNetForSequenceClassification, XLNetModel, AdamW, BertTokenizer, BertForSequenceClassification
+from transformers import XLNetTokenizer, XLNetForSequenceClassification, AdamW, BertTokenizer, BertForSequenceClassification
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
