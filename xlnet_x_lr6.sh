053ea015efcbfcb96577453eefd8a852ee52961c
Yinan Zhou <yinan.a.zhou@gmail.com>
2022-02-25 11:58:35 -0500

    text x batch 3

--- xlnet_x_lr6.sh
+++ xlnet_x_lr6.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:v100l:4
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=4G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --array=25,30,35,40
+#SBATCH --array=40, 45, 50
 #SBATCH --mail-user=yinan.a.zhou@gmail.com
 #SBATCH --mail-type=ALL
 
