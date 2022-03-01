341974e39cbc61eafd93e52ef239918453d6562a
yinan <yinan.a.zhou@gmail.com>
2022-03-01 10:54:43 -0800

    xlnet results batch 1

--- xlnet_x.sh
+++ xlnet_x.sh
@@ -33,5 +33,5 @@ pip install --no-index tokenizers==0.5.2
 wandb login $API_KEY
 
 echo "Starting Task"
-python xlnet_cv.py --ml 512 --bs 8 --epochs 500 --lr 5 --es $SLURM_ARRAY_TASK_ID -lc True
+python xlnet_cv.py --ml 512 --bs 8 --epochs 500 --lr 5 --es $SLURM_ARRAY_TASK_ID --lc True
 
