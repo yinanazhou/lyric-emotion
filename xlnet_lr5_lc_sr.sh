f72a1b7938a30abdbc7dce1df5cc952039036ec0
Yinan Zhou <yinan.a.zhou@gmail.com>
2022-03-11 00:31:32 -0500

    freeze xlnet batch 1

--- xlnet_lr5_lc_sr.sh
+++ xlnet_lr5_lc_sr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=rpp-ichiro
 #SBATCH --time=10:00:00
-#SBATCH --output=run_output/xl_x_lr5_lc_sr_%A_%a.out
+#SBATCH --output=run_output/xl_x_lr7_lc_sr_%A_%a.out
 #SBATCH --gres=gpu:v100l:4
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=4G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
@@ -33,6 +33,6 @@ pip install --no-index tokenizers==0.5.2
 wandb login $API_KEY
 
 echo "Starting Task"
-python xlnet_cv.py --ml 512 --bs 8 --epochs 500 --lr 5 --es $SLURM_ARRAY_TASK_ID --lc True --stop True
+python xlnet_cv.py --ml 512 --bs 8 --epochs 500 --lr 7 --es $SLURM_ARRAY_TASK_ID --lc True --stop True
 
 
