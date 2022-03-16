4e5720ce92ed861beeb6966d2ee7b03d10b1e178
Yinan Zhou <yinan.a.zhou@gmail.com>
2022-03-16 00:07:53 -0400

    adjust

--- submit_job.sh
+++ submit_job.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 array=(
-  xlnet_lr5_lc.sh
-  xlnet_lr5_lc_lemma.sh
-  xlnet_lr5_lc_nr_sr.sh
-  xlnet_lr5_lc_nr_sr_lemma.sh
-  xlnet_lr5_lc_nr_sr_stem.sh
-  xlnet_lr5_lc_sr.sh
-  xlnet_lr5_lc_sr_lemma.sh
-  xlnet_lr5_lc_sr_stem.sh
-  xlnet_lr5_lc_stem.sh
-  xlnet_lr5_lemma.sh
-  xlnet_lr5_nr.sh
-  xlnet_lr5_nr_lemma.sh
-  xlnet_lr5_nr_sr.sh
-  xlnet_lr5_nr_sr_lemma.sh
-  xlnet_lr5_nr_sr_stem.sh
-  xlnet_lr5_nr_stem.sh
-  xlnet_lr5_sr.sh
-  xlnet_lr5_sr_lemma.sh
-  xlnet_lr5_sr_stem.sh
-  xlnet_lr5_stem.sh
-  xlnet_lr5_x.sh
+  # xlnet_lr5_lc.sh
+  # xlnet_lr5_lc_lemma.sh
+  # xlnet_lr5_lc_nr_sr.sh
+  # xlnet_lr5_lc_nr_sr_lemma.sh
+  # xlnet_lr5_lc_nr_sr_stem.sh
+  # xlnet_lr5_lc_sr.sh
+  # xlnet_lr5_lc_sr_lemma.sh
+  # xlnet_lr5_lc_sr_stem.sh
+  # xlnet_lr5_lc_stem.sh
+  # xlnet_lr5_lemma.sh
+  # xlnet_lr5_nr.sh
+  # xlnet_lr5_nr_lemma.sh
+  # xlnet_lr5_nr_sr.sh
+  # xlnet_lr5_nr_sr_lemma.sh
+  # xlnet_lr5_nr_sr_stem.sh
+  # xlnet_lr5_nr_stem.sh
+  # xlnet_lr5_sr.sh
+  # xlnet_lr5_sr_lemma.sh
+  # xlnet_lr5_sr_stem.sh
+  # xlnet_lr5_stem.sh
+  # xlnet_lr5_x.sh
   xlnet_lr6_lc.sh
   xlnet_lr6_lc_lemma.sh
   xlnet_lr6_lc_nr_sr.sh
