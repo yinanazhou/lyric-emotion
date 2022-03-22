ebb209433839b6552b5950e818963510bbac1140
Yinan Zhou <yinan.a.zhou@gmail.com>
2022-03-21 23:50:23 -0400

    sup 02

--- xlnet_lr6_lc_nr_stem.sh
+++ xlnet_lr6_lc_nr_stem.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=rpp-ichiro
 #SBATCH --time=1-00:00:00
-#SBATCH --output=run_output/xl_x_lr6_sr_lemma_%A_%a.out
+#SBATCH --output=run_output/xl_x_lr6_lc_nr_stem_%A_%a.out
 #SBATCH --gres=gpu:v100l:4
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=6G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --array=5
+#SBATCH --array=5,10
 #SBATCH --mail-user=yinan.a.zhou@gmail.com
 #SBATCH --mail-type=ALL
 
@@ -33,6 +33,6 @@ pip install --no-index tokenizers==0.5.2
 wandb login $API_KEY
 
 echo "Starting Task"
-python xlnet_cv.py --ml 512 --bs 8 --epochs 500 --lr 6 --es $SLURM_ARRAY_TASK_ID --stop True --lemma True
+python xlnet_cv.py --ml 512 --bs 8 --epochs 500 --lr 6 --es $SLURM_ARRAY_TASK_ID --lc True --nr True --stem True
 
 
